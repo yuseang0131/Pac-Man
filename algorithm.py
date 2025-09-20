9df035f5a452bba18fe3bd8cd27754b6a60de6a6
yuseang0131 <yuseang0131@gmail.com>
2025-09-20 14:50:59 +0900

    add algorithm file

---
+++ algorithm.py
@@ -0,0 +1,26 @@
+import math
+
+
+class Ghost:
+    pass
+
+
+
+
+# red
+class Blinky(Ghost):
+    pass
+
+# pink
+class Pinky(Ghost):
+    pass
+
+# cyan
+class Lnky(Ghost):
+    pass
+
+# orange
+class Clyde(Ghost):
+    pass
+
+
